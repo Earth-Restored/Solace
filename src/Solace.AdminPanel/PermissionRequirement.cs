791920a5e9494a576e0c56063b3000a36824443a
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2026-07-29 10:55:57 +0200

    Remove AdminPanel project

--- src/Solace.AdminPanel/PermissionRequirement.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace Solace.AdminPanel;
-
-internal sealed class PermissionRequirement(string permission) : IAuthorizationRequirement
-{
-    public string Permission { get; } = permission;
-}
--- src/Solace.AdminPanel/PublicEndpointInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Solace.AdminPanel;
-
-internal sealed class PublicEndpointInfo
-{
-    public string? Locator { get; init; }
-
-    public string? AuthServer { get; init; }
-}
